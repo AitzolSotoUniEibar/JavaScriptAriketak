d5b174ad4a6448d9456449a8ea5fdf6c646502b9
AitzolSotoUniEibar <116349384+AitzolSotoUniEibar@users.noreply.github.com>
2023-09-27 12:19:03 +0200

    Argazkien ariketa

---
+++ ArgazkienAriketa/Argazkiak.js
@@ -0,0 +1,17 @@
+function argazkiak_array_init(){
+  var irudiak = new Array()
+
+  irudiak[0] = new Image()
+  irudiak[0].src = 'img/shrek0.jpg'
+
+  irudiak[1] = new Image()
+  irudiak[1].src = 'img/shrek1.jpg'
+
+  irudiak[2] = new Image()
+  irudiak[2].src = 'img/shrek2.jpg'
+
+  irudiak[3] = new Image()
+  irudiak[3].src = 'img/shrek3.jpg'
+
+  return irudiak
+}
