7a71576b81f04151d21195b6d38bad037a1c6217
AitzolSotoUniEibar <116349384+AitzolSotoUniEibar@users.noreply.github.com>
2023-10-11 11:39:40 +0200

    Web bezero

---
+++ JQueryAriketa9/scripta.js
@@ -0,0 +1,15 @@
+$("#ajaxButton").click(function () {
+  alert("Klik");
+  /* estilo honetako url-a erabiliko zen
+  /*var url = 'http://localhost:8888/login';*/
+  var url = "edukia-ajax.json";
+  $.ajax({
+    type: "GET",
+    url: url,
+    success: function (data) {
+      $("#izena").html(data.user_name);
+      $("#pasahitza").html(data.password);
+      $("#ongietorri").css("display", "block");
+    }
+  });
+});
